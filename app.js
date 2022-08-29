eb8546c33291ebb8d2f98f6eb4f15b06d265d426
Yasin Yavuz <barbaros1675@gmail.com>
2022-08-30 01:21:28 +0200

    arreys done

--- app.js
+++
@@ -1,7 +0,0 @@
-const colors = ["Red", "Yellow", "Green", "Blue", "Pink", "Green", "Green"];
-
-console.log(colors.indexOf("Green")); // 2
-
-console.log(colors.indexOf("Green", 4)); // 5
-
-console.log(colors.indexOf("green")); // -1
